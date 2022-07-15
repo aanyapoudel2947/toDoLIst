6900cebce7d6d6dfd1b95dc05d7850b3998c25fd
aanyapoudel <aanyadog37@gmail.com>
2022-07-15 15:16:59 -0700

    Initial Commit

---
+++ toDoLIst/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  toDoLIst
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
