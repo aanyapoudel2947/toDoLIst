356ed391e242ce1f8ec4ddd63bc9a2cf19d6946f
aanyapoudel <aanyadog37@gmail.com>
2022-07-15 16:25:34 -0700

    wowww

---
+++ toDoLIst/toDoLIst/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  toDoLIst
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name=" OH NO ITS BROKEN"
+    var important = true
+    
+}
