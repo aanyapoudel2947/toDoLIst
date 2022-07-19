71087480bb35570ad5c1d13003bd5cc8af1dbd5f
aanyapoudel <aanyadog37@gmail.com>
2022-07-19 10:54:45 -0700

    laka

--- toDoLIst/toDoLIst/ToDoTableViewController.swift
+++ toDoLIst/toDoLIst/ToDoTableViewController.swift
@@ -9,22 +9,64 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
 
-    var ToDos : [ToDo] = []
+    var ToDos : [ToDoCD] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        //ToDos = createToDos()
+        getToDos()
+        
+        }
+    override func viewWillAppear(_ animated: Bool) {
+        getToDos()
+        
+    
+    }
+    
 
+   
+    func getToDos() {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+          func getToDos() {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+
+              if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                      ToDos = coreDataToDos
+                      tableView.reloadData()
+                  }
+              }
+            }
+          }
+
+      }
+    
+    
+
+    
+    func createToDos ()-> [ToDo]{
+       
+       let crime = ToDo()
+       crime.name = " listen too music"
+       
+       
+       let dog = ToDo()
+       dog.name = "sneak a dog onto campus"
+        dog.important = false
+        
+        return [crime,dog]
+    }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+    
+    
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,19 +77,41 @@ class ToDoTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-       
-        let todo = ToDos[indexPath.row]
         
-        if todo.important{
-            cell.textLabel?.text = "🎧"+todo.name
-        } else {
-            cell.textLabel?.text = todo.name
-        }
-            return cell
+        let toDo = ToDos[indexPath.row]
+        
+        if let name = toDo.name{
+            if toDo.important{
+                cell.textLabel?.text = "🎧" + (toDo.name)!
+            } else {
+                cell.textLabel?.text = toDo.name
+            }
+                
     
-   
-
+        }
+        return cell
+    }//end of tableView
+    
+    override func prepare( for segue: UIStoryboardSegue, sender: Any?) {
+  if let addVC = segue.destination as? AddToDoViewController{
+      addVC.previousVC = self
+  }
+        
+    if let completeVC = segue.destination as? completeToDoViewController {
+    if let toDo = sender as? ToDo{
+        completeVC.selectedToDo = toDo
+        completeVC.previousVC = self
+    }
+    }
+        
+    }//end of prepare
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let toDo = ToDos[indexPath.row]
+        performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
+        
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -94,5 +158,6 @@ class ToDoTableViewController: UITableViewController {
     */
 
 
-    }
+    
 }
+
